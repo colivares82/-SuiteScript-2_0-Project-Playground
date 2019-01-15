b86483184a4bf2be9b6a745769b927f9105af595
Carlos Olivares <carlos.olivares.ve@gmail.com>
2019-01-15 12:50:18 +0100

    first commit

---
+++ FileCabinet/SuiteScripts/sdr_ur_employee.js
@@ -0,0 +1,30 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType UserEventScript
+ */
+
+define([],
+  function () {
+    function getFromUtility(param) {
+      return param * param
+    }
+
+    function myAfterSubmit(context) {
+      var number = 2;
+      var stringNumber = '4';
+      console.log('context->' + context);
+      console.log(getFromUtility(number));
+      console.log(getFromUtility(parseInt(stringNumber)));
+    }
+
+    function myBeforeLoad(context) {
+      log.debug('Hello World', + context);
+    }
+
+    return {
+      afterSubmit: myAfterSubmit,
+      beforeLoad: myBeforeLoad
+      // This are "Entry Points", what means Hooks (afterSubmit, beforeLoad...), there are several of them
+    };
+  });
+
